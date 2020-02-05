3f2fd7b91ece7dfe1714cd9cdc4a9804c6b9698a
Miguel Peralta <eudysperalta723@gmail.com>
2020-02-05 14:26:02 -0800

    actions and reducers

--- treasure-hunt/src/App.js
+++ treasure-hunt/src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 import { StateProvider } from "./contexts/StateContext";
 import { rootReducer, initialState } from "./reducers";
 import { traverse } from "./util/shortestPath";
+import {mine} from "./actions/miningActions"
 
 function App() {
-  traverse(329);
+  //374 warp
+  //55 well
+  //495 transmor
+  //1 shop
+  //
+  traverse(374);
   // walkBack(path);
+  // mine();
+
   return (
     <StateProvider initialState={initialState} reducer={rootReducer}>
       <div className="App">Treasure Hunt</div>
---
+++ treasure-hunt/src/actions/miningActions.js
@@ -0,0 +1,36 @@
+import { axiosWithAuth } from "../util/axiosAuth";
+import { sha256 } from "js-sha256";
+
+
+export const getLastProof = () => {
+  return axiosWithAuth()
+    .get("bc/last_proof/")
+    .then(res => res.data)
+    .catch(err => console.log(err.response));
+};
+export const findProofOfWork = (lastProof, difficulty) => {
+  let proof = 0;
+  let dif = "0".repeat(difficulty);
+  while (sha256(`${lastProof}${proof}`).slice(0, difficulty) !== dif) {
+    proof++;
+  }
+  return proof;
+};
+export const mine = async () => {
+  console.log("getting last proof...");
+  let lastProof = await getLastProof();
+  //   console.log('last proof: ', lastProof);
+  let { proof, difficulty, cooldown } = lastProof;
+  //   wait(cooldown)
+  //   console.log('proof: ', proof);
+  console.log("finding proof of work...");
+  let workingProof = findProofOfWork(proof, difficulty);
+  console.log("proof of work found!");
+  return axiosWithAuth()
+    .post("bc/mine/", { proof: workingProof })
+    .then(res => {
+      console.log("proof of work submission result: ", res.data);
+      return res.data;
+    })
+    .catch(err => console.log(err.response));
+};
