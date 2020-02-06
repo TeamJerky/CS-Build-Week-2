aa7b714572804ddfe8b086999b10514c00ddf8e0
May Ng <mngmay@gmail.com>
2020-02-05 17:52:11 -0800

    automated snitching

--- treasure-hunt/src/util/shortestPath.js
+++ treasure-hunt/src/util/shortestPath.js
@@ -1,6 +1,6 @@
 import { map } from "./map";
 import { initGame } from "../data";
-import { walkBack, moveBoost, wait } from "./traverse";
+import { walkBack, wait } from "./traverse";
 
 function goToRoomById(startingRoom, graph, roomId) {
   console.log("STARTING ROOM BFS", startingRoom);
@@ -25,12 +25,24 @@ function goToRoomById(startingRoom, graph, roomId) {
   }
 }
 
-export async function traverse(target) {
+export async function traverse(target, map) {
   let room = await initGame();
   wait(room.cooldown);
   let path = goToRoomById(map[room.room_id], map, target);
   console.log("room", room, "path", path);
-  await walkBack(path);
+  return await walkBack(path);
+}
+
+// if the direction is the same, you can dash quickly
+// '{"direction":"n", "num_rooms":"5", "next_room_ids":"10,19,20,63,72"}'
+function dash(start, path, map) {
+  // grab existing path
+  // for room in path
+  // if the direction is the same, dash
+  let newPath = [...path];
+  let startingRoom = map[start.room_id];
+  for (let room in path) {
+  }
 }
 
 export default goToRoomById;
