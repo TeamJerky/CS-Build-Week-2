5a1dc996319d14ebdca5f714363f2f9086c6e895
May Ng <mngmay@gmail.com>
2020-02-05 17:55:38 -0800

    snitches get stitches

--- treasure-hunt/src/App.js
+++ treasure-hunt/src/App.js
@@ -1,14 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { StateProvider } from "./contexts/StateContext";
 import { rootReducer, initialState } from "./reducers";
 import { traverse } from "./util/shortestPath";
+import { map } from "./util/map";
+import { darkmap } from "./util/darkworldmap";
+import { mine, autoSnitchMiner } from "./actions/miningActions";
+
+import { axiosWithAuth } from "./util/axiosAuth";
 
 function App() {
-  traverse(356);
+  // traverse(0);
+  // traverseDarkWorld(555);
+  // mine();
+  const [darkRoom, setDarkRoom] = useState(null);
+
+  const examine = () => {
+    return axiosWithAuth()
+      .post("adv/examine/", { name: "Wishing Well" })
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response));
+  };
+
+  const updateDarkRoom = event => {
+    setDarkRoom(event.target.value);
+  };
+
+  const darkWorld = event => {
+    event.preventDefault();
+    traverse(+darkRoom, darkmap);
+  };
+
+  const goToWell = () => {
+    traverse(555, darkmap);
+  };
+
+  const getSnitch = () => {
+    return axiosWithAuth()
+      .post("adv/take/", { name: "golden snitch" })
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response));
+  };
+
+  autoSnitchMiner();
 
   return (
     <StateProvider initialState={initialState} reducer={rootReducer}>
       <div className="App">Treasure Hunt</div>
+      <form onSubmit={darkWorld}>
+        <label htmlFor="dark">
+          Dark Room Travel:
+          <input
+            type="number"
+            name="dark"
+            id="dark"
+            onChange={updateDarkRoom}
+          />
+        </label>
+        <button>Travel</button>
+      </form>
+      <button onClick={goToWell}>Go To Well</button>
+      <button onClick={examine}>Examine</button>
+      <button onClick={getSnitch}>Take 'golden snitch'</button>
     </StateProvider>
   );
 }
--- treasure-hunt/src/actions/miningActions.js
+++ treasure-hunt/src/actions/miningActions.js
@@ -1,16 +1,21 @@
-import { axiosWithAuth } from './util/axiosAuth';
-import { sha256 } from 'js-sha256';
+import { axiosWithAuth } from "../util/axiosAuth";
+import { sha256 } from "js-sha256";
+import { compiledCPU } from "../util/compiledCPU";
+import { initGame } from "../data";
+import { wait } from "../util/traverse";
+import { darkmap } from "../util/darkworldmap";
+import { traverse } from "../util/shortestPath";
 
 export const getLastProof = () => {
   return axiosWithAuth()
-    .get('bc/last_proof/')
+    .get("bc/last_proof/")
     .then(res => res.data)
     .catch(err => console.log(err.response));
 };
 
 export const findProofOfWork = (lastProof, difficulty) => {
   let proof = 0;
-  let dif = '0'.repeat(difficulty);
+  let dif = "0".repeat(difficulty);
   while (sha256(`${lastProof}${proof}`).slice(0, difficulty) !== dif) {
     proof++;
   }
@@ -18,20 +23,77 @@ export const findProofOfWork = (lastProof, difficulty) => {
 };
 
 export const mine = async () => {
-  console.log('getting last proof...');
+  console.log("getting last proof...");
   let lastProof = await getLastProof();
   //   console.log('last proof: ', lastProof);
   let { proof, difficulty, cooldown } = lastProof;
   //   wait(cooldown)
   //   console.log('proof: ', proof);
-  console.log('finding proof of work...');
+  console.log("finding proof of work...");
   let workingProof = findProofOfWork(proof, difficulty);
-  console.log('proof of work found!');
+  console.log("proof of work found!");
   return axiosWithAuth()
-    .post('bc/mine/', { proof: workingProof })
+    .post("bc/mine/", { proof: workingProof })
     .then(res => {
-      console.log('proof of work submission result: ', res.data);
+      console.log("proof of work submission result: ", res.data);
       return res.data;
     })
     .catch(err => console.log(err.response));
+};
+
+const examine = () => {
+  return axiosWithAuth()
+    .post("adv/examine/", { name: "Wishing Well" })
+    .then(res => res.data.description)
+    .catch(err => console.log(err.response));
+};
+
+const getSnitch = () => {
+  return axiosWithAuth()
+    .post("adv/take/", { name: "golden snitch" })
+    .then(res => {
+      if (
+        res.data.messages[0] ===
+        "A great warmth floods your body as your hand closes around the snitch before it vanishes."
+      ) {
+        console.log("Success!", res.data);
+      } else {
+        console.log("Failed :(", res.data);
+      }
+      return res.data;
+    })
+    .catch(err => console.log(err.response));
+};
+
+// Automated call
+export const autoSnitchMiner = async () => {
+  let cpu = new compiledCPU();
+  // First init to get starting room
+  let init = await initGame();
+
+  wait(init.cooldown);
+  while (true) {
+    // Go to well (rm 555)
+    if (init.room_id !== 555) {
+      let well = await traverse(555, darkmap);
+      console.log("WELL", well);
+      wait(well.cooldown);
+    }
+    // Examine (res.data.description === string to decode)
+    let message = await examine();
+    wait(message.cooldown);
+    // Decode to get room #
+    cpu.load(message);
+
+    let room_number = cpu.run();
+    console.log("room number", room_number);
+    // traverse to room
+    let snitch_room = await traverse(+room_number, darkmap);
+    wait(snitch_room.cooldown);
+    // pick up snitch
+    let snitch = await getSnitch();
+    console.log("SNITCH", snitch);
+    wait(snitch.cooldown);
+    // loop;
+  }
 };
